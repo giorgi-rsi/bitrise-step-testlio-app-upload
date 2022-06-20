311030a895c127e316a44774e55ccd572ec5ea08
Giorgi Peranidze <peranidze@rushstreetinteractive.com>
2022-06-20 11:59:31 +0300

    add step

--- step.sh
+++ step.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+TESTLIO_AUTHORIZATION=${authorization}
+PROJECT_ID=${project_id}
+APP_NAME=${name}
+APP_VERSION=${version}
+APP_FILE=${file}
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+curl --request POST https://api.testlio.com/build/v3/collections/$PROJECT_ID/builds \
+    -H 'Content-type: multipart/form-data' \
+    -H "Authorization: $TESTLIO_AUTHORIZATION" \
+    -F name=$APP_NAME \
+    -F version=$APP_VERSION \
+    -F file=$APP_FILE
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+exit 0
